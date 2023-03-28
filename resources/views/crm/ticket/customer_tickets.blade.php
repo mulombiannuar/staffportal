4f0bf3b39f9966b5695f7ebf90689bf3ae9058a2
Mulombi Anuary <mulombi.anuary@BIMASKENYA.local>
2023-03-28 16:26:00 +0300

    modified customer tickets

--- resources/views/crm/ticket/customer_tickets.blade.php
+++ resources/views/crm/ticket/customer_tickets.blade.php
@@ -11,7 +11,7 @@
                             Add Customer Ticket</a></li>
 
                     {{-- /. branch manager/credit officer --}}
-                    @if (Auth::user()->hasRole('bimas staff|branch manager'))
+                    @if (Auth::user()->hasRole('credit officer|branch manager'))
                         <li class="nav-item"><a class="nav-link" href="#creditofficer" data-toggle="tab"><i
                                     class="fa fa-list"></i>
                                 Credit Officer ({{ count($creditofficer) }})</a></li>
@@ -240,63 +240,63 @@
                     <!-- /.tab-pane -->
 
                     <!--/. Credit officer-->
-                    @if (Auth::user()->hasRole('bimas staff|branch manager'))
-                    <div class="tab-pane" id="creditofficer">
-                        <div class="card card-warning">
-                            <div class="card-header">
-                                <h3 class="card-title"><i class="fa fa-list"></i> Credit Officer </h3>
-                            </div>
-                            <div class="card-body">
-                                <table id="table1"
-                                    class="table table-sm table-bordered table-striped table-head-fixed">
-                                    <thead>
-                                        <tr>
-                                            <th>S.N</th>
-                                            <th>TICKET ID</th>
-                                            <th>NAMES</th>
-                                            <th>MOBILE</th>
-                                            <th>RESIDENCE</th>
-                                            <th>BUSINESS</th>
-                                            <th>DATE</th>
-                                            <th>BRANCH</th>
-                                            <th>OFFICER</th>
-                                            <th>CREATED AT</th>
-                                            <th>ACTIONS</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($creditofficer as $ticket)
+                    @if (Auth::user()->hasRole('credit officer|branch manager'))
+                        <div class="tab-pane" id="creditofficer">
+                            <div class="card card-warning">
+                                <div class="card-header">
+                                    <h3 class="card-title"><i class="fa fa-list"></i> Credit Officer </h3>
+                                </div>
+                                <div class="card-body">
+                                    <table id="table1"
+                                        class="table table-sm table-bordered table-striped table-head-fixed">
+                                        <thead>
                                             <tr>
-                                                <td>{{ $loop->iteration }}</td>
-                                                <td>{{ $ticket->ticket_uuid }}</td>
-                                                <td>{{ strtoupper($ticket->customer_name) }}</td>
-                                                <td>{{ $ticket->customer_phone }}</td>
-                                                <td>{{ $ticket->residence }}</td>
-                                                <td>{{ $ticket->business }}</td>
-                                                <td>{{ $ticket->date_raised }}</td>
-                                                <td>{{ $ticket->outpost_name }}</td>
-                                                <td>{{ $ticket->officer_name }}</td>
-                                                <td>{{ $ticket->created_at }}</td>
-                                                <td>
-                                                    <div class="margin">
-                                                        <div class="btn-group">
-                                                            <a href="{{ route('crm.tickets.show', $ticket->ticket_id) }}"
-                                                                title="Click to view ticket details">
-                                                                <button type="button" class="btn btn-xs btn-info"><i
-                                                                        class="fa fa-eye"></i>
-                                                                    View</button>
-                                                            </a>
-                                                        </div>
-                                                    </div>
-                                                </td>
+                                                <th>S.N</th>
+                                                <th>TICKET ID</th>
+                                                <th>NAMES</th>
+                                                <th>MOBILE</th>
+                                                <th>RESIDENCE</th>
+                                                <th>BUSINESS</th>
+                                                <th>DATE</th>
+                                                <th>BRANCH</th>
+                                                <th>OFFICER</th>
+                                                <th>CREATED AT</th>
+                                                <th>ACTIONS</th>
                                             </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($creditofficer as $ticket)
+                                                <tr>
+                                                    <td>{{ $loop->iteration }}</td>
+                                                    <td>{{ $ticket->ticket_uuid }}</td>
+                                                    <td>{{ strtoupper($ticket->customer_name) }}</td>
+                                                    <td>{{ $ticket->customer_phone }}</td>
+                                                    <td>{{ $ticket->residence }}</td>
+                                                    <td>{{ $ticket->business }}</td>
+                                                    <td>{{ $ticket->date_raised }}</td>
+                                                    <td>{{ $ticket->outpost_name }}</td>
+                                                    <td>{{ $ticket->officer_name }}</td>
+                                                    <td>{{ $ticket->created_at }}</td>
+                                                    <td>
+                                                        <div class="margin">
+                                                            <div class="btn-group">
+                                                                <a href="{{ route('crm.tickets.show', $ticket->ticket_id) }}"
+                                                                    title="Click to view ticket details">
+                                                                    <button type="button" class="btn btn-xs btn-info"><i
+                                                                            class="fa fa-eye"></i>
+                                                                        View</button>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
                             </div>
-                            <!-- /.card-body -->
                         </div>
-                    </div>
                     @endif
                     <!-- /.tab-pane -->
 
@@ -363,13 +363,73 @@
 
                     <!--/. Senior Manager -->
                     @if ($senior_manager || Auth::user()->hasRole('general manager') || Auth::user()->hasRole('chief executive officer'))
+                        @php
+                            $manager_tickets = [];
+                            $ticket_title = 'Senior Manager Tickets';
+                        @endphp
+                        @if (Auth::user()->hasRole('finance manager'))
+                            @php
+                                $manager_tickets = $financemanager;
+                                $ticket_title = 'Finance Manager Tickets';
+                            @endphp
+                        @endif
+
+                        @if (Auth::user()->hasRole('ict manager'))
+                            @php
+                                $manager_tickets = $ictmanager;
+                                $ticket_title = 'ICT Manager Tickets';
+                            @endphp
+                        @endif
+
+                        @if (Auth::user()->hasRole('audit manager'))
+                            @php
+                                $manager_tickets = $auditmanager;
+                                $ticket_title = 'AudiT Manager Tickets';
+                            @endphp
+                        @endif
+
+                        @if (Auth::user()->hasRole('legal manager'))
+                            @php
+                                $manager_tickets = $legalmanager;
+                                $ticket_title = 'Legal Manager Tickets';
+                            @endphp
+                        @endif
+
+                        @if (Auth::user()->hasRole('credits manager'))
+                            @php
+                                $manager_tickets = $creditsmanager;
+                                $ticket_title = 'Credits Manager Tickets';
+                            @endphp
+                        @endif
+
+                        @if (Auth::user()->hasRole('marketing manager'))
+                            @php
+                                $manager_tickets = $marketingmanager;
+                                $ticket_title = 'Marketing Manager Tickets';
+                            @endphp
+                        @endif
+
+                        @if (Auth::user()->hasRole('operations manager'))
+                            @php
+                                $manager_tickets = $operationsmanager;
+                                $ticket_title = 'Operations Manager Tickets';
+                            @endphp
+                        @endif
+
+                        @if (Auth::user()->hasRole('human resource manager'))
+                            @php
+                                $manager_tickets = $humanresourcemanager;
+                                $ticket_title = 'Human Resource Manager Tickets';
+                            @endphp
+                        @endif
+
                         <div class="tab-pane" id="seniormanagers">
                             <div class="card card-warning">
                                 <div class="card-header">
-                                    <h3 class="card-title"><i class="fa fa-list"></i> Senior Managers </h3>
+                                    <h3 class="card-title"><i class="fa fa-list"></i> {{ $ticket_title }} </h3>
                                 </div>
                                 <div class="card-body">
-                                    <table id="table2"
+                                    <table id="table3"
                                         class="table table-sm table-bordered table-striped table-head-fixed">
                                         <thead>
                                             <tr>
@@ -387,40 +447,6 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            @php
-                                                $manager_tickets = [];
-                                            @endphp
-                                            @if (Auth::user()->hasRole('finance manager'))
-                                                $manager_tickets = $financemanager;
-                                            @endif
-
-                                            @if (Auth::user()->hasRole('ict manager'))
-                                                $manager_tickets = $ictmanager;
-                                            @endif
-
-                                            @if (Auth::user()->hasRole('audit manager'))
-                                                $manager_tickets = $auditmanager;
-                                            @endif
-
-                                            @if (Auth::user()->hasRole('legal manager'))
-                                                $manager_tickets = $legalmanager;
-                                            @endif
-
-                                            @if (Auth::user()->hasRole('credits manager'))
-                                                $manager_tickets = $creditsmanager;
-                                            @endif
-
-                                            @if (Auth::user()->hasRole('marketing manager'))
-                                                $manager_tickets = $marketingmanager;
-                                            @endif
-
-                                            @if (Auth::user()->hasRole('operations manager'))
-                                                $manager_tickets = $financemanager;
-                                            @endif
-
-                                            @if (Auth::user()->hasRole('human resource manager'))
-                                                $manager_tickets = $humanresourcemanager;
-                                            @endif
 
                                             @foreach ($manager_tickets as $ticket)
                                                 <tr>
